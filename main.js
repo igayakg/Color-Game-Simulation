bf803779a4aeea5398432c926877c5119dd3c357
edrill <edrillb@gmail.com>
2025-01-25 18:33:41 +0800

    Updated overflow behavior

--- main.js
+++ main.js
@@ -14,6 +14,15 @@ function simulate() {
         { round: 6, spin: 'Yellow', ret1: 20, ret2: 30, ret3: 40, outcome: 'Win', balance: 120, final: 130 },
         { round: 7, spin: 'Red', ret1: 10, ret2: 20, ret3: 30, outcome: 'Win', balance: 100, final: 110 },
         { round: 8, spin: 'Green', ret1: 15, ret2: 25, ret3: 35, outcome: 'Lose', balance: 90, final: 95 },
+        { round: 9, spin: 'Yellow', ret1: 20, ret2: 30, ret3: 40, outcome: 'Win', balance: 120, final: 130 },
+        { round: 1, spin: 'Red', ret1: 10, ret2: 20, ret3: 30, outcome: 'Win', balance: 100, final: 110 },
+        { round: 2, spin: 'Green', ret1: 15, ret2: 25, ret3: 35, outcome: 'Lose', balance: 90, final: 95 },
+        { round: 3, spin: 'Yellow', ret1: 20, ret2: 30, ret3: 40, outcome: 'Win', balance: 120, final: 130 },
+        { round: 4, spin: 'Red', ret1: 10, ret2: 20, ret3: 30, outcome: 'Win', balance: 100, final: 110 },
+        { round: 5, spin: 'Green', ret1: 15, ret2: 25, ret3: 35, outcome: 'Lose', balance: 90, final: 95 },
+        { round: 6, spin: 'Yellow', ret1: 20, ret2: 30, ret3: 40, outcome: 'Win', balance: 120, final: 130 },
+        { round: 7, spin: 'Red', ret1: 10, ret2: 20, ret3: 30, outcome: 'Win', balance: 100, final: 110 },
+        { round: 8, spin: 'Green', ret1: 15, ret2: 25, ret3: 35, outcome: 'Lose', balance: 90, final: 95 },
         { round: 9, spin: 'Yellow', ret1: 20, ret2: 30, ret3: 40, outcome: 'Win', balance: 120, final: 130 }
     ];
 
